00dc060f126cf749cc0beea14d50167b044e21c7
cywwee <kyrylo1buryk.it@gmail.com>
2024-07-15 23:01:37 +0200

    kolya responses 'n mentioned

--- mainds.py
+++ mainds.py
@@ -1,4 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+import json
 
-print("kolya skachay project")
+try:
+    with open('config.json', 'r', encoding='utf-8') as file:
+        config = json.load(file)
+except (FileNotFoundError, json.JSONDecodeError) as e:
+    print(f"Ошибка при загрузке конфигурации: {e}")
+    exit(1)
 
-print("у меня все работает николя")
+token_ds = config["token_ds"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(intents=intents, command_prefix="$")
+
+@bot.command()
+async def random_word(ctx):
+    random_response = random.choice(config["responses"])
+    await ctx.send(random_response)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+    if bot.user.mentioned_in(message):
+        random_response = random.choice(config["responses"])
+        await message.reply(random_response)
+
+    if "слава украине" in message.content.lower():
+        await message.reply("в составе РОССИИ!!!")
+
+    if "иди нахуй" in message.content.lower():
+        await message.reply("Своим помахуй")
+
+bot.run(token_ds)
